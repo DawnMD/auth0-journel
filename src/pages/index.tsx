40eb5a448f13aa14d54d703e1dae756c8a209158
Mainak Das <mainakdas104@gmail.com>
2023-04-01 12:13:55 +0530

    Chore: Check auth func

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import { type NextPage } from "next";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  const { user, isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded && isSignedIn) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      {isSignedIn && <SignOutButton />}
+      {!isSignedIn && <SignInButton />}
+      {isSignedIn && <p>{user.fullName}</p>}
+    </div>
+  );
 };
 
 export default Home;
